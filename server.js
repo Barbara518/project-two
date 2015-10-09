bc88375466f45475abe9aa648ba4c553f46be573
Barbara Rodriguez-Zamora <barbararodriguezzamora@gmail.com>
2015-10-09 10:39:46 -0400

    more heroku stuff

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-var express = require('express'),
-	PORT	= process.env.PORT || 5432,
-	server	= express();
+var express  = require('express'),
+	PORT	 = process.env.PORT || 5432,
+	server	 = express(),
+	MONGOURI = process.env.MONGOLAB_URI,
+	dbname	 = "useful",
+	mongoose = require('mongoose');
 
 
 server.get('/', function (req, res) {
@@ -8,6 +11,8 @@ server.get('/', function (req, res) {
 	res.end();
 });
 
+mongoose.connect(MONGOURI + "/" + dbname);
+
 server.listen(PORT, function (){
-	console.log('Yasss')
+	console.log('Yasss', PORT)
 })
